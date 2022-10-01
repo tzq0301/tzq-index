fb855b256f233565871b2df3a5d30b2d15464a25
tzq0301 <1661610563@qq.com>
2022-10-02 01:54:32 +0800

    reinit

---
+++ src/views/BlogView.vue
@@ -0,0 +1,10 @@
+<template>
+  Blog
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
+<template>
+  Home
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
