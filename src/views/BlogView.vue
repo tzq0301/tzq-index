ef35ee0359ef4a0537768ad0fea70f9dad1446ac
tzq0301 <1661610563@qq.com>
2022-10-03 22:14:33 +0800

    feat(blog): mark blog as coming soon

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -3,13 +3,15 @@
     <div class="mx-auto w-2/5 text-gray-700">
       <div class="text-3xl text-black my-12 font-bold">Blog</div>
 
-      <BlogYearBlock year="2022">
+      Coming soon!
 
-      </BlogYearBlock>
+<!--      <BlogYearBlock year="2022">-->
 
-      <BlogYearBlock year="2021">
+<!--      </BlogYearBlock>-->
 
-      </BlogYearBlock>
+<!--      <BlogYearBlock year="2021">-->
+
+<!--      </BlogYearBlock>-->
     </div>
   </div>
 </template>
